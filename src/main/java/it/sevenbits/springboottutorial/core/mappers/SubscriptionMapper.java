1dba85d5fd478637379a3778eed30129b211cfbd
Ilya Siganov <ilya.blan4@gmail.com>
2015-07-07 19:58:17 +0600

    add postgresql support and remove hsql support. Also replace hsql specific migration on psql migration.

--- src/main/java/it/sevenbits/springboottutorial/core/mappers/SubscriptionMapper.java
+++ src/main/java/it/sevenbits/springboottutorial/core/mappers/SubscriptionMapper.java
@@ -7,14 +7,14 @@ import java.util.List;
 
 public interface SubscriptionMapper {
 
-    @Select("SELECT id, name, email FROM subscriptions")
+    @Select("SELECT id, subscriber_name, email FROM subscriptions")
     @Results({
         @Result(column = "id", property = "id"),
         @Result(column = "email", property = "email"),
-        @Result(column = "name", property = "name")
+        @Result(column = "subscriber_name", property = "name")
     })
     List<Subscription> findAll();
 
-    @Insert("INSERT INTO subscriptions (email, name) VALUES (#{email}, #{name})")
+    @Insert("INSERT INTO subscriptions (email, subscriber_name) VALUES (#{email}, #{name})")
     void save(final Subscription subscription);
 }
